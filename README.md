acea3895e1be3bb877b5a497e04e74ffb8022257
Shoham Chakraborty <shhmchk@gmail.com>
2022-10-08 21:09:32 +0800

    remove foundry files, add solc-json for mythril

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Deployment Information
 - Proxy Owner:
 - Proxy:
 - Implementation:
+
+Run Mythril: `docker run -v $(pwd):/tmp mythril/myth analyze /tmp/src/eUSD.sol --solc-json /tmp/remapping.json`
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,7 @@ async function main() {
   const EUSD = await ethers.getContractFactory("eUSD");
   const eUSD = await upgrades.deployProxy(EUSD, [
     "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", // USDC Address
-    134400, // Withdrawal Block Timeout
+    100800, // Withdrawal Block Timeout
   ]);
   await eUSD.deployed();
 
